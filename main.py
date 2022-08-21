b989816cab56ecc8a54296e66eed105add207f40
junglestory <monocaine@gmail.com>
2022-08-21 16:03:06 +0900

    목록 조회 기능 추가

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
+import string
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.params import Depends 
+from sqlalchemy.orm import Session
+from db.connection import get_db
+from models import News
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
+def hello(db: Session = Depends(get_db)):
+    # logging.debug("hello world")
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/news")
+def news_list(db: Session = Depends(get_db)):
+    datas = db.query(News).all()
+    
+    return datas
+
+
+@app.get("/news/{journal_id}")
+def news_journal(journal_id: str, db: Session = Depends(get_db)):    
+    datas = db.query(News).filter(News.journal_id == journal_id).all()
+
+    return datas
